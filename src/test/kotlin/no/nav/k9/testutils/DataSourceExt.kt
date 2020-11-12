6a266cfbab61c23a365a6c11a80cbafa0cc09b05
Anders Kristiansen <anders.kristiansen@nav.no>
2020-11-12 10:32:14 +0100

    testdatasource

--- src/test/kotlin/no/nav/k9/testutils/DataSourceExt.kt
+++ src/test/kotlin/no/nav/k9/testutils/DataSourceExt.kt
@@ -1,7 +1,22 @@
 package no.nav.k9.testutils
 
+import com.opentable.db.postgres.embedded.EmbeddedPostgres
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import javax.sql.DataSource
+
+fun testDataSource(embeddedPostgres: EmbeddedPostgres): HikariDataSource {
+    val hikariConfig = HikariConfig().apply {
+        this.jdbcUrl = embeddedPostgres.getJdbcUrl("postgres", "postgres")
+        maximumPoolSize = 3
+        minimumIdle = 1
+        idleTimeout = 10001
+        connectionTimeout = 1000
+        maxLifetime = 30001
+    }
+    return HikariDataSource(hikariConfig)
+}
 
 internal fun DataSource.cleanAndMigrate() {
     Flyway
