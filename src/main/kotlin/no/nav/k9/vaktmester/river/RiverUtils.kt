8fa1affab2b23a220ef585e167e6f47719acf1b3
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-11-19 17:52:57 +0100

    Gjør endringer slik at vi håndterer meldinger i feilende test.

--- src/main/kotlin/no/nav/k9/vaktmester/river/RiverUtils.kt
+++ src/main/kotlin/no/nav/k9/vaktmester/river/RiverUtils.kt
@@ -2,6 +2,7 @@ package no.nav.k9.vaktmester.river
 
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.k9.rapid.behov.Behovsformat
 import no.nav.k9.rapid.river.requireArray
@@ -10,9 +11,11 @@ import no.nav.k9.rapid.river.requireText
 import no.nav.k9.vaktmester.db.Arkiv
 import java.time.ZonedDateTime
 import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.isMissingOrNull
 import no.nav.k9.vaktmester.db.InFlight
 
 internal const val Løsninger = "@løsninger"
+private val objectMapper = jacksonObjectMapper()
 
 internal data class Meldingsinformasjon(
     internal val sistEndret: ZonedDateTime,
@@ -31,7 +34,7 @@ internal fun JsonMessage.vaktmesterOppgave(): JsonMessage {
     require(Behovsformat.CorrelationId) { it.requireText() }
     require(Behovsformat.SistEndret) { ZonedDateTime.parse(it.asText()) }
     require(Behovsformat.Behov) { it.requireObject() }
-    require(Løsninger) { it.requireObject() }
+    interestedIn(Løsninger)
     require(Behovsformat.Behovsrekkefølge) { it.requireArray() }
     return this
 }
@@ -39,7 +42,10 @@ internal fun JsonMessage.vaktmesterOppgave(): JsonMessage {
 internal fun JsonMessage.meldingsinformasjon() = Meldingsinformasjon(
     sistEndret = ZonedDateTime.parse(get(Behovsformat.SistEndret).asText()),
     behov = get(Behovsformat.Behov) as ObjectNode,
-    løsninger = get(Løsninger) as ObjectNode,
+    løsninger = when (get(Løsninger).isMissingOrNull()) {
+        true -> objectMapper.createObjectNode()
+        false -> get(Løsninger) as ObjectNode
+    },
     behovssekvensId = get(Behovsformat.Id).asText(),
     correlationId = get(Behovsformat.CorrelationId).asText(),
     behovsrekkefølge = (get(Behovsformat.Behovsrekkefølge) as ArrayNode).map { it.asText()!! }
