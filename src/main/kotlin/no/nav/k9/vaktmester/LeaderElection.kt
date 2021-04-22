814a37f98a0fc9a19f26ee2b9040a7f596658a7c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-04-22 13:55:28 +0200

    manglet protocol på electorPath

--- src/main/kotlin/no/nav/k9/vaktmester/LeaderElection.kt
+++ src/main/kotlin/no/nav/k9/vaktmester/LeaderElection.kt
@@ -3,6 +3,7 @@ package no.nav.k9.vaktmester
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
 import java.net.InetAddress
+import java.net.URI
 import java.net.URL
 
 // https://doc.nais.io/addons/leader-election/
@@ -10,12 +11,14 @@ internal object LeaderElection {
     internal fun isLeader(): Boolean {
         val electorPath = System.getenv("ELECTOR_PATH")
 
+        logger.info("ElectorPath=[$electorPath]")
+
         return when (electorPath.isNullOrBlank()) {
             true -> {
                 logger.info("Environment variabel ELECTOR_PATH er ikke satt").let { true }
             }
             false -> {
-                val leader = JSONObject(URL(electorPath).readText()).getString("name")
+                val leader = JSONObject(URL("http://$electorPath").readText()).getString("name")
                 val hostname = InetAddress.getLocalHost().hostName
                 logger.info("Leader=[$leader], Hostname=[$hostname]")
                 return hostname == leader
@@ -24,5 +27,4 @@ internal object LeaderElection {
     }
 
     private val logger = LoggerFactory.getLogger(LeaderElection::class.java)
-
 }
