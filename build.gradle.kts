eaa2cc781c75285d35c8718ea9d6e52ec98f8bf0
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2023-05-09 10:45:03 +0200

    Dep+gradle bumps, slettet snyk workflow (#269)
    * Dep+gradle bumps, slettet snyk workflow
    
    * Dep+gradle bumps, slettet snyk workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,29 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val k9rapidVersion = "1.20230316100804-072b34b"
-val flywayVersion = "9.16.0"
+val k9rapidVersion = "1.20230426095941-a725ff7"
+val flywayVersion = "9.17.0"
 val hikariVersion = "5.0.1"
 val kotliqueryVersion = "1.9.0"
-val postgresVersion = "42.5.4"
-val ktorVersion = "2.2.4"
-val dusseldorfVersion = "3.2.2.4-98ccf55"
+val postgresVersion = "42.6.0"
+val ktorVersion = "2.3.0"
+val dusseldorfVersion = "3.2.3.0-f979367"
 val vaultJdbcVersion = "1.3.10"
 val orgJsonVersion = "20230227"
 
 // Test avhengigheter
-val junitJupiterVersion = "5.9.2"
+val junitJupiterVersion = "5.9.3"
 val embeddedPostgres = "2.0.3"
 val embeddedPostgresBinaries = "12.9.0"
-val mockkVersion = "1.13.4"
+val mockkVersion = "1.13.5"
 val assertjVersion = "3.24.2"
 val jsonassertVersion = "1.5.1"
 
 val mainClass = "no.nav.k9.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("com.github.johnrengelman.shadow") version "8.1.0"
+    kotlin("jvm") version "1.8.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.sonarqube") version "4.0.0.2929"
     jacoco
 }
@@ -107,7 +107,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "8.0.2"
+        gradleVersion = "8.1.1"
     }
 
 }
