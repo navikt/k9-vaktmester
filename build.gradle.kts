bb10b5679e9bd93f43e9a028c97ae7a514838218
albrektsson <emil.albrektsson@nav.no>
2023-12-18 14:49:08 +0100

    Dep + gradle bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val k9rapidVersion = "1.20231002100147-90c2022"
-val flywayVersion = "9.22.3"
+val flywayVersion = "10.3.0"
 val hikariVersion = "5.1.0"
 val kotliqueryVersion = "1.9.0"
-val postgresVersion = "42.6.0"
-val ktorVersion = "2.3.6"
-val dusseldorfVersion = "4.1.1"
+val postgresVersion = "42.7.1"
+val ktorVersion = "2.3.7"
+val dusseldorfVersion = "4.1.3"
 val vaultJdbcVersion = "1.3.10"
 val orgJsonVersion = "20231013"
 
 // Test avhengigheter
 val junitJupiterVersion = "5.10.1"
-val embeddedPostgres = "2.0.4"
+val embeddedPostgres = "2.0.6"
 val embeddedPostgresBinaries = "12.9.0"
 val mockkVersion = "1.13.8"
 val assertjVersion = "3.24.2"
@@ -21,7 +21,7 @@ val jsonassertVersion = "1.5.1"
 val mainClass = "no.nav.k9.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.sonarqube") version "4.4.1.3373"
     jacoco
@@ -44,8 +44,7 @@ dependencies {
 
     // Database
     implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    //implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
     implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
     implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
     runtimeOnly("org.postgresql:postgresql:$postgresVersion")
@@ -84,6 +83,7 @@ tasks {
         testLogging {
             events("passed", "skipped", "failed")
         }
+        finalizedBy(jacocoTestReport) // report is always generated after tests run
     }
 
     withType<ShadowJar> {
@@ -99,21 +99,16 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "8.4"
+        gradleVersion = "8.5"
     }
 
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
-    reports {
-        xml.required.set(true)
-        csv.required.set(false)
+    withType<JacocoReport> {
+        dependsOn(test) // tests are required to run before generating the report
+        reports {
+            xml.required.set(true)
+            csv.required.set(false)
+        }
     }
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 
 sonarqube {
