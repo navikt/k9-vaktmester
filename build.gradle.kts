1ca6d7fc21acd82abc9ce34591b0b00da2198350
Helga Nyrud <45488635+helganyrud@users.noreply.github.com>
2025-10-06 13:49:39 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val k9rapidVersion = "1.20251001090432-58d7f09"
-val flywayVersion = "11.13.2"
+val flywayVersion = "11.12.0"
 val hikariVersion = "7.0.2"
 val kotliqueryVersion = "1.9.1"
 val postgresVersion = "42.7.8"
