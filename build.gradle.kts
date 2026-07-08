99285b1050cdbc037d7dc1eb3e0b14d87d55dd87
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2026-07-08 16:11:28 +0200

    Ruller tilbake flyway til 11.12.0 etter at det feilet i dev
    whitespace

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val k9rapidVersion = "1.20260630092344-01f2f22"
-val flywayVersion = "12.10.0"
+val flywayVersion = "11.12.0"
 val hikariVersion = "7.1.0"
 val kotliqueryVersion = "1.9.1"
 val postgresVersion = "42.7.12"
@@ -10,6 +10,7 @@ val dusseldorfVersion = "8.0.2"
 val vaultJdbcVersion = "1.3.10"
 val orgJsonVersion = "20260522"
 val nettyVersion = "4.2.15.Final"
+
 
 // Test avhengigheter
 val junitJupiterVersion = "6.1.1"
